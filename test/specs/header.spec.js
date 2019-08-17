57b96d3da17c4f9fd0d7516fc21c41cb4daddcc1
Матвей Мурашко <stranger2626@gmail.com>
2019-08-17 19:00:53 +0300

    added a bad example

--- test/specs/header.spec.js
+++ test/specs/header.spec.js
@@ -1,12 +1,24 @@
 const expect = require("chai").expect;
-const HomePageClass = require("../utils/page_objects/home_page");
+const HomePageClass = require("../utils/page_objects/home_page/home_page");
 const HomePage = new HomePageClass();
 
 describe("Home page header", function () {
 
     it("should have 6 menu items", async function () {
         await HomePage.open();
+        await HomePage.Header.navigationButtons.clickElementByText("OUR WORK");
         const countOfNavigationButtons = await HomePage.Header.navigationButtons.getCount();
+        expect(countOfNavigationButtons).to.be.equal(6);
+    });
+
+    // This an example of a bad implementation, don't to it this way EVER!
+    it("should have 6 menu items (Example of how NOT to do it)", async function () {
+        await browser.get("https://www.epam.com");
+        const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
+        const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
+        elementToClick = element.all(by.css("div.header .top-navigation__item")).get(elementToClickIndex);
+        await elementToClick.click();
+        const countOfNavigationButtons = await element.all(by.css("div.header .top-navigation__item")).count();
         expect(countOfNavigationButtons).to.be.equal(6);
     });
 
