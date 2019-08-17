8873f201f7d570fe509661e37a6c29e4b309579b
Матвей Мурашко <stranger2626@gmail.com>
2019-08-17 19:00:13 +0300

    added clickElementByText to collection

--- test/utils/page_objects/base_elements/base_collection.js
+++ test/utils/page_objects/base_elements/base_collection.js
@@ -8,6 +8,14 @@ class Collection {
     getTexts() {
         return this.collection.getText();
     };
+    async clickElementByText(textToClick) {
+        const arrayOfElementTexts = await this.collection.getText();
+        const elementToClickIndex = arrayOfElementTexts.indexOf(textToClick);
+        if (elementToClickIndex === -1) {
+            throw new Error(`No element with [${textToClick}] text found!`);
+        }
+        return this.collection.get(elementToClickIndex).click();
+    };
 };
 
 module.exports = Collection;
