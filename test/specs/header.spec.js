8b6b73af4362aab3428f6bd5c65f41095c3d3d0f
matsvei.murashka <Matsvei_Murashka@epam.com>
2020-02-21 14:39:45 +0300

    added extra wait

--- test/specs/header.spec.js
+++ test/specs/header.spec.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect;
 const PageFactory = require("../utils/page_objects/pageFactory");
-
+const EC = protractor.ExpectedConditions;
 describe("Home page header", function () {
 
     beforeEach(function() {
@@ -11,12 +11,13 @@ describe("Home page header", function () {
     it("should have 6 menu items", async function () {
         await PageFactory.getPage("Home").open();
         await PageFactory.getPage("Home").Header.navigationButtons.clickElementByText("OUR WORK");
+        await browser.sleep(3000);
         const countOfNavigationButtons = await PageFactory.getPage("Home").Header.navigationButtons.getCount();
         expect(countOfNavigationButtons).to.be.equal(6);
     });
 
     // This an example of a bad implementation, don't to it this way EVER!
-    it("should have 6 menu items (Example of how NOT to do it)", async function () {
+    xit("should have 6 menu items (Example of how NOT to do it)", async function () {
         await browser.get("https://www.epam.com");
         const arrayOfelementsTexts = await element.all(by.css("div.header .top-navigation__item")).getText();
         const elementToClickIndex = arrayOfelementsTexts.indexOf("OUR WORK");
